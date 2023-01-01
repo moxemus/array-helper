d70dba0c571c697c44d85814ef8cc205d19f3d81
Maksim Partsiankou <maksim.part@gmail.com>
2023-01-01 23:24:53 +0300

    add PHPdoc comments

--- src/Helper.php
+++ src/Helper.php
@@ -4,6 +4,13 @@ namespace moxemus\array;
 
 class Helper
 {
+
+    /**
+     * Returns real first array element, not with zero index
+     *
+     * @param array $array
+     * @return mixed
+     */
     public static function getFirstElement(array $array): mixed
     {
         if (empty($array)) return null;
@@ -14,11 +21,25 @@ class Helper
         return $array[$firstIndex];
     }
 
+    /**
+     * Returns is array empty without NULL, '' and 0 values
+     *
+     * @param array $array
+     * @return mixed
+     */
     public static function isEmpty(array $array): bool
     {
         return empty(array_filter($array));
     }
 
+    /**
+     * Moves element on any new place in array
+     *
+     * @param array $array
+     * @param $indexFrom
+     * @param $indexTo
+     * @return mixed
+     */
     public static function moveElement(array &$array, $indexFrom, $indexTo): bool
     {
         $fromValue = $array[$indexFrom] ?? null;
