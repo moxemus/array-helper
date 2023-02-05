182bf976f269a7f5ba5104e22faf7f0b86774d0c
Maksim Partsiankou <maksim.part@gmail.com>
2023-02-05 02:21:03 +0100

    add new functions

--- src/Helper.php
+++ src/Helper.php
@@ -4,6 +4,27 @@ namespace moxemus\array;
 
 class Helper
 {
+    /**
+     * Returns real first array value, not with zero index
+     *
+     * @param array $array
+     * @return mixed
+     */
+    public static function getFirstValue(array $array): mixed
+    {
+        return $array[self::getFirstIndex($array)];
+    }
+
+    /**
+     * Returns real last array value, not with count($array)-1 index
+     *
+     * @param array $array
+     * @return string|int|null
+     */
+    public static function getLastValue(array $array): string|int|null
+    {
+        return $array[self::getLastIndex($array)];
+    }
 
     /**
      * Returns real first array element, not with zero index
@@ -11,25 +32,50 @@ class Helper
      * @param array $array
      * @return mixed
      */
-    public static function getFirstElement(array $array): mixed
+    public static function getFirstIndex(array $array): mixed
     {
         if (empty($array)) return null;
 
         $keys = array_keys($array);
-        $firstIndex = $keys[0];
 
-        return $array[$firstIndex];
+        return $keys[0];
     }
 
     /**
-     * Returns is array empty without NULL, '' and 0 values
+     * Returns real last array element, not with zero index
      *
      * @param array $array
-     * @return mixed
+     * @return string|int|null
      */
-    public static function isEmpty(array $array): bool
+    public static function getLastIndex(array $array): string|int|null
     {
-        return empty(array_filter($array));
+        if (empty($array)) return null;
+
+        $keys = array_keys($array);
+
+        return $keys[count($array) - 1];
+    }
+
+    /**
+     * Returns index of max value in array
+     *
+     * @param array $array
+     * @return string|int
+     */
+    public static function getMaxIndex(array $array): string|int
+    {
+        return array_keys($array, max($array))[0];
+    }
+
+    /**
+     * Returns index of miv value in array
+     *
+     * @param array $array
+     * @return string|int
+     */
+    public static function getMinIndex(array $array): string|int
+    {
+        return array_keys($array, min($array))[0];
     }
 
     /**
@@ -55,5 +101,28 @@ class Helper
         $array[$indexTo] = $fromValue;
 
         return true;
+    }
+
+    /**
+     * Returns is array empty without NULL, '' and 0 values
+     *
+     * @param array $array
+     * @return bool
+     */
+    public static function isEmpty(array $array): bool
+    {
+        return empty(array_filter($array));
+    }
+
+    /**
+     * Returns is arrays are equal
+     *
+     * @param array $a
+     * @param array $b
+     * @return bool
+     */
+    public static function isEqual(array $a, array $b): bool
+    {
+        return empty(array_diff($a, $b));
     }
 }
